75e8170289631de339fb9ec8d781cdcd1e113036
Will <834661621@qq.com>
2018-12-26 11:53:42 +0800

    HttpClient初步封装完成

--- app/src/main/java/com/will/myutils/MainActivity.java
+++ app/src/main/java/com/will/myutils/MainActivity.java
@@ -2,6 +2,16 @@ package com.will.myutils;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.will.commonsdk.http.HttpClient;
+import com.will.commonsdk.http.request.CommonRequest;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        HttpClient.get(CommonRequest.createGetRequest("https://www.imooc.com", null), new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.i("TEST", "HTTPS SUCCESS");
+            }
+        });
     }
 }
---
+++ commonsdk/src/main/java/com/will/commonsdk/http/HttpClient.java
@@ -0,0 +1,45 @@
+package com.will.commonsdk.http;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Crate Time:  2018/12/25
+ * Author:      LiuHanwei
+ * Email:       liuhanwei@xhg.com
+ * Description:
+ */
+public class HttpClient {
+    private static final int TIME_OUT = 30;
+    private static OkHttpClient mHttpClient;
+
+    static {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+
+        builder.connectTimeout(TIME_OUT, TimeUnit.SECONDS);
+        builder.readTimeout(TIME_OUT, TimeUnit.SECONDS);
+        builder.writeTimeout(TIME_OUT, TimeUnit.SECONDS);
+        builder.followRedirects(true);
+
+        mHttpClient = builder.build();
+    }
+
+    public static Call get(Request request, Callback callback) {
+        Call call = mHttpClient.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+}
